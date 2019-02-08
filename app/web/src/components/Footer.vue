5387f7814050e29f06edb8d8554a97fb1b8296de
vijayaneja <42745903+vijayaneja@users.noreply.github.com>
2019-02-07 18:00:17 -0800

    Update Footer.vue
    s

--- app/web/src/components/Footer.vue
+++ app/web/src/components/Footer.vue
@@ -14,7 +14,7 @@
       <div class="col-lg-6">
       </div>
       <div class="col-lg-12 credits">
-        Vijay Test Azure Global Blackbelt Team 2
+        Vijay Test Azure Global Blackbelt Team 2666
       </div>
       <div class="col-lg-6">
       </div>
